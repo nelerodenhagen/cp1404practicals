ba8efed4ffb17ff4fd7211b06a7ce8ba30cca90f
nelerodenhagen <nele.rodenhagen@my.jcu.edu.au>
2023-11-18 14:27:28 +0800

    Refactored score program

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,25 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    print(f"With the score {score} you reached: ", calculate_grade(score))
+    random_score = random.randint(1, 100)
+    print(f"With the score {random_score} you reached: ", calculate_grade(random_score))
+
+
+def calculate_grade(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Pass"
+    else:
+        return "Bad"
+
+
+main()
