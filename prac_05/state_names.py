f4486e6bf4b6147a89db07354c3dfd077ede86a9
nelerodenhagen <nele.rodenhagen@my.jcu.edu.au>
2023-12-07 18:17:42 +0800

    complete prac 5 hex color exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,7 +4,6 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 for code, name in CODE_TO_NAME.items():
@@ -16,4 +15,4 @@ while state_code != "":
         print(state_code, "is", CODE_TO_NAME[state_code])
     except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
