dc23c66f9aa9448ee5fb47de982380d13b2c4802
nelerodenhagen <nele.rodenhagen@my.jcu.edu.au>
2024-01-10 16:21:40 +0800

    added car, taxi and taxi test and tested taxi

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,16 @@
+"""
+testing the taxi class
+"""
+from taxi import Taxi
+
+
+def main():
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+
+
+main()
