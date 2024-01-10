b7a4d4a16f8b2d62160f4c0f34cae07ce8de0911
nelerodenhagen <nele.rodenhagen@my.jcu.edu.au>
2024-01-10 17:10:51 +0800

    added silver service taxi and tested it

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,12 @@
+from silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    my_silver_taxi = SilverServiceTaxi("Hummer", 200, 4)
+    print(my_silver_taxi)
+    another_silver_taxi = SilverServiceTaxi("Maike", 300, 2)
+    another_silver_taxi.drive(18)
+    print(f"the fare is: ${another_silver_taxi.get_fare()}")
+
+
+main()
--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,10 +7,10 @@ from taxi import Taxi
 def main():
     my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
-    print(my_taxi)
+    print(f"{my_taxi} and the fare is ${my_taxi.get_fare()}")
     my_taxi.start_fare()
     my_taxi.drive(100)
-    print(my_taxi)
+    print(f"{my_taxi} and the fare is ${my_taxi.get_fare()}")
 
 
 main()
